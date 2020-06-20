d89059bff6ca2883af2998e5b69433e33767ea2a
Jawaid Mushtaq <jawaid_mushtaq@yahoo.com>
2020-06-20 23:18:40 +0800

    OL Vector Tile Layer

--- openlayers/workshop/main.js
+++ openlayers/workshop/main.js
@@ -1,134 +1,32 @@
 import 'ol/ol.css';
-import {fromLonLat} from 'ol/proj';
 import {Map, View} from 'ol';
-import {Vector as VectorLayer, Tile as TileLayer} from 'ol/layer';
-import {Vector as VectorSource, Stamen} from 'ol/source';
-import Feature from 'ol/Feature';
-import Point from 'ol/geom/Point';
-import Renderer from 'ol/renderer/webgl/PointsLayer';
-import {clamp} from 'ol/math';
+import MVT from 'ol/format/MVT';
+import VectorTileLayer from 'ol/layer/VectorTile';
+import VectorTileSource from 'ol/source/VectorTile';
+import {fromLonLat} from 'ol/proj';
 
-const source = new VectorSource();
-
-const client = new XMLHttpRequest();
-client.open('GET', 'data/meteorites.csv');
-client.onload = function() {
-  const csv = client.responseText;
-  const features = [];
-
-  let prevIndex = csv.indexOf('\n') + 1; // scan past the header line
-
-  let curIndex;
-  while ((curIndex = csv.indexOf('\n', prevIndex)) != -1) {
-    const line = csv.substr(prevIndex, curIndex - prevIndex).split(',');
-    prevIndex = curIndex + 1;
-
-    const coords = fromLonLat([parseFloat(line[4]), parseFloat(line[3])]);
-    if (isNaN(coords[0]) || isNaN(coords[1])) {
-      // guard against bad data
-      continue;
-    }
-
-    features.push(new Feature({
-      mass: parseFloat(line[1]) || 0,
-      year: parseInt(line[2]) || 0,
-      geometry: new Point(coords)
-    }));
-  }
-  source.addFeatures(features);
-};
-client.send();
-
-const minYear = 1850;
-const maxYear = 2015;
-const span = maxYear - minYear;
-const rate = 10; // years per second
-
-const start = Date.now();
-let currentYear = minYear;
-
-const color = [1, 0, 0, 0.5];
-
-class CustomLayer extends VectorLayer {
-  createRenderer() {
-    return new Renderer(this, {
-      colorCallback: function(feature, vertex, component) {
-        return color[component];
-      },
-      sizeCallback: function(feature) {
-        return 18 * clamp(feature.get('mass') / 200000, 0, 1) + 8;
-      },
-      fragmentShader: `
-          precision mediump float;
-
-          uniform float u_currentYear;
-
-          varying vec2 v_texCoord;
-          varying vec4 v_color;
-          varying float v_opacity;
-
-          void main(void) {
-            float impactYear = v_opacity;
-            if (impactYear > u_currentYear) {
-              discard;
-            }
-
-            vec2 texCoord = v_texCoord * 2.0 - vec2(1.0, 1.0);
-            float sqRadius = texCoord.x * texCoord.x + texCoord.y * texCoord.y;
-
-            float factor = pow(1.1, u_currentYear - impactYear);
-
-            float value = 2.0 * (1.0 - sqRadius * factor);
-            float alpha = smoothstep(0.0, 1.0, value);
-
-            gl_FragColor = v_color;
-            gl_FragColor.a *= alpha;
-          }`,
-
-      opacityCallback: function(feature) {
-        // here the opacity channel of the vertices is used to store the year of impact
-        return feature.get('year');
-      },
-      uniforms: {
-        u_currentYear: function() {
-          return currentYear;
-        }
-      },
-
-
-    });
-  }
-}
+// See https://openmaptiles.com/hosting/ for terms and API key
+// const key = 'lirfd6Fegsjkvs0lshxe'; // use your own instead
+const key = 'PemhNirzKYCxIA7wHsQi'; // use your own instead
 
 const map = new Map({
   target: 'map-container',
-  layers: [
-    new TileLayer({
-      source: new Stamen({
-        layer: 'toner'
-      })
-    }),
-    new CustomLayer({
-      source: source
-    })
-  ],
   view: new View({
-    center: [0, 0],
-    zoom: 2
+    center: fromLonLat([-117.1625, 32.715]),
+    zoom: 6
   })
 });
 
-const yearElement = document.getElementById('year');
-
-function render() {
-  const elapsed = rate * (Date.now() - start) / 1000;
-  currentYear = minYear + (elapsed % span);
-  yearElement.innerText = currentYear.toFixed(0);
-
-  map.render();
-  requestAnimationFrame(render);
-}
-
-render();
-
+const layer = new VectorTileLayer({
+  source: new VectorTileSource({
+    attributions: [
+      '<a href="http://www.openmaptiles.org/" target="_blank">&copy; OpenMapTiles</a>',
+      '<a href="http://www.openstreetmap.org/about/" target="_blank">&copy; OpenStreetMap contributors</a>'
+    ],
+    format: new MVT(),
+    url: `https://free-{1-3}.tilehosting.com/data/v3/{z}/{x}/{y}.pbf.pict?key=${key}`,
+    maxZoom: 14
+  })
+});
+map.addLayer(layer);
 
