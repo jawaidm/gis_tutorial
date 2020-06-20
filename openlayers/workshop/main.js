7cfa8c94145051c0eb15310637f4ee8cedf71415
Jawaid Mushtaq <jawaid_mushtaq@yahoo.com>
2020-06-20 20:31:56 +0800

    OL WebGL Map Setup

--- openlayers/workshop/main.js
+++ openlayers/workshop/main.js
@@ -1,54 +1,58 @@
 import 'ol/ol.css';
-import GeoJSON from 'ol/format/GeoJSON';
-import Map from 'ol/Map';
-import VectorLayer from 'ol/layer/Vector';
-import VectorSource from 'ol/source/Vector';
-import View from 'ol/View';
-import sync from 'ol-hashed';
-import DragAndDrop from 'ol/interaction/DragAndDrop';
-import Draw from 'ol/interaction/Draw';
-import Modify from 'ol/interaction/Modify';
+import {fromLonLat} from 'ol/proj';
+import {Map, View} from 'ol';
+import {Vector as VectorLayer, Tile as TileLayer} from 'ol/layer';
+import {Vector as VectorSource, Stamen} from 'ol/source';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
 
 const source = new VectorSource();
-const map = new Map({
-    target: 'map-container',
-    view: new View({
-        center: [0, 0],
-        zoom: 2
+
+const client = new XMLHttpRequest();
+client.open('GET', 'data/meteorites.csv');
+client.onload = function() {
+  const csv = client.responseText;
+  const features = [];
+
+  let prevIndex = csv.indexOf('\n') + 1; // scan past the header line
+
+  let curIndex;
+  while ((curIndex = csv.indexOf('\n', prevIndex)) != -1) {
+    const line = csv.substr(prevIndex, curIndex - prevIndex).split(',');
+    prevIndex = curIndex + 1;
+
+    const coords = fromLonLat([parseFloat(line[4]), parseFloat(line[3])]);
+    if (isNaN(coords[0]) || isNaN(coords[1])) {
+      // guard against bad data
+      continue;
+    }
+
+    features.push(new Feature({
+      mass: parseFloat(line[1]) || 0,
+      year: parseInt(line[2]) || 0,
+      geometry: new Point(coords)
+    }));
+  }
+  source.addFeatures(features);
+};
+client.send();
+
+
+new Map({
+  target: 'map-container',
+  layers: [
+    new TileLayer({
+      source: new Stamen({
+        layer: 'toner'
+      })
+    }),
+    new VectorLayer({
+      source: source
     })
+  ],
+  view: new View({
+    center: [0, 0],
+    zoom: 2
+  })
 });
 
-const layer = new VectorLayer({
-      source: source
-});
-
-map.addInteraction(new DragAndDrop({
-      source: source,
-      formatConstructors: [GeoJSON]
-}));
-
-map.addInteraction(new Modify({
-      source: source
-}));
-
-map.addInteraction(new Draw({
-      type: 'Polygon',
-      source: source
-}));
-
-const clear = document.getElementById('clear');
-clear.addEventListener('click', function() {
-  source.clear();
-});
-
-const format = new GeoJSON({featureProjection: 'EPSG:3857'});
-const download = document.getElementById('download');
-source.on('change', function() {
-  const features = source.getFeatures();
-  const json = format.writeFeatures(features);
-  download.href = 'data:text/json;charset=utf-8,' + json;
-});
-
-map.addLayer(layer);
-
-sync(map);
