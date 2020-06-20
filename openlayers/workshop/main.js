4ad49d72e0bf5f32aa41e441f0ae2bfef0449ccb
Jawaid Mushtaq <jawaid_mushtaq@yahoo.com>
2020-06-20 23:40:12 +0800

    OL Render elevation data

--- openlayers/workshop/main.js
+++ openlayers/workshop/main.js
@@ -1,32 +1,36 @@
 import 'ol/ol.css';
-import {Map, View} from 'ol';
-import MVT from 'ol/format/MVT';
-import VectorTileLayer from 'ol/layer/VectorTile';
-import VectorTileSource from 'ol/source/VectorTile';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import XYZSource from 'ol/source/XYZ';
 import {fromLonLat} from 'ol/proj';
 
-// See https://openmaptiles.com/hosting/ for terms and API key
-// const key = 'lirfd6Fegsjkvs0lshxe'; // use your own instead
-const key = 'PemhNirzKYCxIA7wHsQi'; // use your own instead
+const key = 'pk.eyJ1Ijoiam11c2h0YXEiLCJhIjoiY2tibnN5NnZsMDdwNTJxcG91YmlxcjdkNiJ9.fovPgbwp-Uj-NS-AOfkJww';
 
-const map = new Map({
+const elevation = new XYZSource({
+  url: 'https://api.mapbox.com/v4/mapbox.terrain-rgb/{z}/{x}/{y}.pngraw?access_token=' + key,
+  crossOrigin: 'anonymous'
+});
+
+
+new Map({
   target: 'map-container',
-  view: new View({
-    center: fromLonLat([-117.1625, 32.715]),
-    zoom: 6
-  })
-});
+  layers: [
+    new TileLayer({
+      source: new XYZSource({
+        url: 'http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg'
+      })
+    }),
+    new TileLayer({
+      opacity: 0.8,
+      source: elevation
+    })
 
-const layer = new VectorTileLayer({
-  source: new VectorTileSource({
-    attributions: [
-      '<a href="http://www.openmaptiles.org/" target="_blank">&copy; OpenMapTiles</a>',
-      '<a href="http://www.openstreetmap.org/about/" target="_blank">&copy; OpenStreetMap contributors</a>'
-    ],
-    format: new MVT(),
-    url: `https://free-{1-3}.tilehosting.com/data/v3/{z}/{x}/{y}.pbf.pict?key=${key}`,
-    maxZoom: 14
+  ],
+  view: new View({
+    center: fromLonLat([-117.1493, 32.6965]), // San Diego
+    //center: fromLonLat([74.32936, 31.582045]), //lahore
+    zoom: 12
   })
 });
-map.addLayer(layer);
 
