c21b0b87e8cef11486f958619c9288ecf1be4791
Jawaid Mushtaq <jawaid_mushtaq@yahoo.com>
2020-06-20 19:55:45 +0800

    OL Downloading Features

--- openlayers/workshop/main.js
+++ openlayers/workshop/main.js
@@ -36,6 +36,19 @@ map.addInteraction(new Draw({
       source: source
 }));
 
+const clear = document.getElementById('clear');
+clear.addEventListener('click', function() {
+  source.clear();
+});
+
+const format = new GeoJSON({featureProjection: 'EPSG:3857'});
+const download = document.getElementById('download');
+source.on('change', function() {
+  const features = source.getFeatures();
+  const json = format.writeFeatures(features);
+  download.href = 'data:text/json;charset=utf-8,' + json;
+});
+
 map.addLayer(layer);
 
 sync(map);
